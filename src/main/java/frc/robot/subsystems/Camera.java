814eb603051db546cde39899a328a31f0407ecef
Sam <samantha.epshteyn@gmail.com>
2026-01-24 15:20:44 -0500

    Found changes in position for limelight alignment

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -1,15 +1,20 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.RawFiducial;
+import frc.robot.RobotContainer.moveStraight;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.controller.*;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
 
 public class Camera extends SubsystemBase {
     private static double statX;
@@ -23,7 +28,7 @@ public class Camera extends SubsystemBase {
     private static double[] tagToCamera;
 
     public void startCamera() {
-        NetworkTable armTable = NetworkTableInstance.getDefault().getTable("limelight-algae");
+        NetworkTable armTable = NetworkTableInstance.getDefault().getTable("limelight-shooter");
         NetworkTableEntry tx = armTable.getEntry("tx");
         NetworkTableEntry ty = armTable.getEntry("ty");
         NetworkTableEntry ta = armTable.getEntry("ta");
@@ -57,6 +62,8 @@ public class Camera extends SubsystemBase {
         
         SmartDashboard.putNumber("Tag ID", aprilTagID);
 
+        alignTag(1, 1, 2, 30);
+
     }
 
 
@@ -64,7 +71,8 @@ public class Camera extends SubsystemBase {
         //System.out.println(detected_ID);
         return detected_ID;
     }
-    
+
+    //gets degree value from crosshairs("How many degrees do we need to turn to line up?")
     public static double getX(){
         return statX;
     }
@@ -77,22 +85,78 @@ public class Camera extends SubsystemBase {
         return statY;
     }
 
+    //gets distance value, camera is 0,0,0("How far do we need to move left/right to line up?")
     public static double getDistX(){
-        return NetworkTableInstance.getDefault().getTable("limelight-algae").getEntry("targetpose_cameraspace").getDoubleArray(new double[6])[0];
+        return NetworkTableInstance.getDefault().getTable("limelight-shooter").getEntry("targetpose_cameraspace").getDoubleArray(new double[6])[0];
     }
 
     public static double getDistYaw(){
-        return NetworkTableInstance.getDefault().getTable("limelight-algae").getEntry("targetpose_cameraspace").getDoubleArray(new double[6])[4];
+        return NetworkTableInstance.getDefault().getTable("limelight-shooter").getEntry("targetpose_cameraspace").getDoubleArray(new double[6])[4];
     }
 
     public static double getDistZ(){
-        return NetworkTableInstance.getDefault().getTable("limelight-algae").getEntry("targetpose_cameraspace").getDoubleArray(new double[6])[2];
+        return NetworkTableInstance.getDefault().getTable("limelight-shooter").getEntry("targetpose_cameraspace").getDoubleArray(new double[6])[2];
+    }
+    
+    //gets distance value, camera is 0,0,0("How far do we need to move left/right to line up?")
+    public static double getDistZFromTag(){
+        return NetworkTableInstance.getDefault().getTable("limelight-shooter").getEntry("botpose_targetspace").getDoubleArray(new double[6])[2];
     }
 
+    public static double getDistXFromTag(){
+        return NetworkTableInstance.getDefault().getTable("limelight-shooter").getEntry("botpose_targetspace").getDoubleArray(new double [6])[0];
+    }
+     //gets degree value
+    public static double getYawFromTag(){
+        return NetworkTableInstance.getDefault().getTable("limelight-shooter").getEntry("botpose_targetspace").getDoubleArray(new double [6])[4];
+    }
 
-    public void alignTag(double tagID, double distance){
-        //Pose2d current_position = 
-        //use functions from limelight helpers as examples or just copy?
+    public void alignTag(double tagID, double xTarget, double zTarget, double yawTarget){
+        double zPosition = getDistZFromTag();
+        double xPosition = getDistXFromTag();
+        double yawPosition = getYawFromTag();
+
+        double xPositionChange = xPosition + xTarget;
+        double zPositionChange = zPosition+zTarget;
+        double yawPositionChange = yawPosition + yawTarget;
+
+        double totalDistanceToMove = Math.sqrt(Math.pow(zPositionChange,2) + Math.pow(xPositionChange, 2) );
+        double totalYaw = Math.atan2(xPositionChange,-zPositionChange);
+        totalYaw = totalYaw*(180/Math.PI);
+        totalYaw = totalYaw - yawPosition; 
+        
+
+        SmartDashboard.putNumber("X Position: ",xPosition);
+        SmartDashboard.putNumber("Z position: " ,zPosition);
+        SmartDashboard.putNumber("Yaw: ",yawPosition);
+        SmartDashboard.putNumber("X Position change: ",xPositionChange);
+        SmartDashboard.putNumber("Z position change: " ,zPositionChange);
+        SmartDashboard.putNumber("Yaw position change: " ,yawPositionChange);
+        SmartDashboard.putNumber("Direction to drive in: ", totalDistanceToMove);
+        SmartDashboard.putNumber("angle to drive at: ",totalYaw);
+
+
+
+
+
+
+
+        //Pose3d current_position = LimelightHelpers.getBotPose3d_wpiBlue("limelight-shooter");
+        //if (tagID == detected_ID){
+            //if (xDegrees != 500 && xDistance != 500){
+                //while (xDistance != NetworkTableInstance.getDefault().getTable("limelight-shooter").getEntry("targetpose_cameraspace").getDoubleArray(new double[6])[0]){
+                    //double distanceToMove = NetworkTableInstance.getDefault().getTable("limelight-shooter").getEntry("targetpose_cameraspace").getDoubleArray(new double[6])[0] - xDistance;
+                    //if (distanceToMove>0){
+                          //;
+                   //}
+                    
+                //}
+            //}
+            //while (NetworkTableInstance.getDefault().getTable("limelight-shooter").getEntry("targetpose_cameraspace").getDoubleArray(new double[6])[2]!= distance){
+                
+
+        //}
+        
     }
 
     
